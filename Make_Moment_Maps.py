38a34684639d0f2bc6a9adf532845934befa9f90
NadiaSteyn <65017580+NadiaSteyn@users.noreply.github.com>
2021-03-03 15:26:27 +0200

    Add files via upload
    An update to my script Make Moment Maps.

--- Make_Moment_Maps.py
+++ Make_Moment_Maps.py
@@ -15,25 +15,29 @@ from astropy.io import fits  # We use fits to open the actual data file
 from spectral_cube import SpectralCube
 import matplotlib.gridspec as gridspec
 import warnings
+from astropy.wcs import WCS
 from astropy.wcs import FITSFixedWarning
 warnings.simplefilter('ignore', FITSFixedWarning)
+#module add python/3.8.3
+#comment out block: cmd + /
+#cancel the run control+c
 
 ######################################
 #FITS cube
 ######################################
 
 #Choose FITS cube:
-infile_fits = "/Users/nadia/data/mosaic1.fits"
 
-hdu = fits.open(infile_fits)
+hdu = fits.open('/carta_share/users/nadia/Nadia_GA/1538811061_HI_mosaic_heliocen.fits') # shape: 3788, 3474, 573 
 cube = SpectralCube.read(hdu) # Initiate a SpectralCube
-freq_check = False
-if cube.header['CTYPE3'] == 'FREQ':
-    print('!!! CUBE IN FREQ UNITS, CONVERTING INTO VELOCITY UNITS !!!')
-    freq_check = True
-cube = cube.with_spectral_unit(u.m/u.s,velocity_convention='radio',rest_value=1420.4*u.MHz) 
-hdu.close()  
-#Cube must have dimensions [Vel, Dec, RA]
+
+#convert cube to VRAD m/s:
+print('CONVERTING CUBE INTO VELOCITY UNITS...')
+cube = cube.with_spectral_unit(u.m/u.s,velocity_convention='radio',rest_value=1420.4*u.MHz)
+print(f"Cube now has spectral unit {cube.header['CTYPE3']} {cube.header['CUNIT3']}")
+hdu.close()
+
+#Cube now has dimensions [Vel, Dec, RA]
 _, Dec, _ = cube.world[0, :, 0]  #extract Dec world coordinates from cube
 _, _, RA = cube.world[0, 0, :]  #extract RA world coordinates from cube
 
@@ -45,17 +49,16 @@ _, _, RA = cube.world[0, 0, :]  #extract RA world coordinates from cube
 rel = np.ones(1000000) # a fake array of rels in case the the user doesn't have rels
 ID = np.ones(1000000)
 
-infile = "/Users/nadia/data/Final_detection_lists/Mosaic1_new_final/Mosaic1_Vis_Poslogcat_matches_64.txt" 
+#Choose detection list:
+infile = '/users/nadia/moment_maps/1538811061_mosaic/Visual_sources/1538811061_visual.txt'
+vel, x, y, z = np.loadtxt(infile,usecols=(6,7,8,9),unpack=True) #vel in km/s
+vel = np.around(vel,0)
 
-ID, rel, x, y, z, x_vis, y_vis, z_vis, ra_h, ra_m, ra_s, dec_d, dec_m, dec_s, vel_sof = np.loadtxt(infile,usecols=range(1,16),unpack=True)
-if freq_check == True:#assuming that if cube has freq units sofia will also have freq units
-    vel_sof = 300000*((-vel_sof + 1420.4e6)/(1420.4e6))
-xpad=10
-ypad=10
-zpad=20
-
-ra_sof = (ra_h + ra_m/60 + ra_s/(60*60)) * (360/24)
-dec_sof = (dec_d - dec_m/60 - dec_s/(60*60))
+#vel = 300000*((-freq + 1420.4e6)/(1420.4e6)) #if your list has freq
+xpad=35
+ypad=35
+zpad=2
+radii=15 #radius used around source for HI profile
 
 ######################################
 
@@ -72,12 +75,11 @@ if type(ID)==float or type(ID)==int:
 else:
     ID = ID[:len(x)]
 
-
 pos_vels = []
 neg_vels = []
-pos_test = np.where(vel_sof > -300)[0]
-for i in range(len(vel_sof)):
-    if vel_sof[i]>-300: 
+pos_test = np.where(vel > -300)[0]
+for i in range(len(vel)):
+    if vel[i]>-300: 
         pos_vels.append(i)
     else: 
         neg_vels.append(i)
@@ -100,17 +102,16 @@ def reduce_fits_file(fits_file,x_center,y_center,z_center,xpad,ypad,zpad): #if c
                              zerofy(int(round(x_center-xpad))):int(round(x_center+xpad+1))]
     return sub_array_3d
 
-def noise_cube(cube,x,y,z,xpad,ypad,zpad):
+def noise_cube(cube,x,y,z,xpad,ypad,zpad): # for the masking
     sub_cube = reduce_fits_file(cube, x, y, z, xpad, ypad, zpad) # Cutting the main fits file into a sub-cube
     ### This is how we determine what method we want to use for noise
     noise_moment_0 = sub_cube.moment(order=0)
-    
-    noise= np.nanmean(noise_moment_0.hdu.data)  #nanmean takes the mean ignoring the nans 
+    noise= np.nanmean(noise_moment_0.hdu.data)  # nanmean takes the mean ignoring the nans 
     sigma = np.nanstd(noise_moment_0.hdu.data)
     return noise,sigma
 
 def get_noise(cube,x,y,z,xpad,ypad,zpad):  # function to look at the 8 squares around the main square 
-    means=[] #lists to store the mean and sigma values 
+    means=[] # lists to store the mean and sigma values 
     sigmas=[]
     x_vals = x+np.array([-1,-1,-1,0,0,1,1,1])*2*xpad    # x and y positions of the 8 squares 
     y_vals = y+np.array([1,0,-1,1,-1,1,0,-1])*2*ypad
@@ -130,24 +131,23 @@ def get_noise(cube,x,y,z,xpad,ypad,zpad):  # function to look at the 8 squares a
     return noise,sigma  #return the noise and sigma
 
 
-def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='world',clean=False, cleaning_factor=1,order=0,vmin=None,vmax=None,HI_profile=False,HI_radii=5):
+def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='world',clean=False, cleaning_factor=1,order=0,vmin=None,vmax=None,HI_profile=False,HI_radii=radii):
     
     current_wcs_3d = cube.wcs
     current_ra,current_dec,current_vel = current_wcs_3d.pixel_to_world_values(x,y,z)
-    current_vel = current_vel/1000 # Converting to km/s
+    current_vel = current_vel/1000 # in km/s
     print('Cutting moment map...')
     sub_cube = reduce_fits_file(cube, x, y, z, xpad, ypad, zpad) # Cutting the main fits file into a sub-cube
     current_sub_cube_wcs_3d = sub_cube.wcs
     sub_cube_center_x,sub_cube_center_y,_ = current_sub_cube_wcs_3d.world_to_pixel_values(current_ra,current_dec,current_vel)
-    #going from sofia's pixel, to ra/dec, to pixel in the sub cube
+    #going from the given pixel, to ra/dec, to pixel in the sub cube
     if order ==0:
-        moment_map = sub_cube.with_spectral_unit(u.km/u.s).moment(order=order)  # changing units from m/s to km/s. Units will be cube units x spectral unit (Jy km/s / beam)
-        print(np.mean(moment_map))
+        moment_map = sub_cube.with_spectral_unit(u.km/u.s).moment(order=order)  # changing units from m/s to km/s. Units will be cube units * spectral unit (Jy km/s / beam)
     else:
         moment_map=sub_cube.with_spectral_unit(u.km/u.s).moment(order=1,axis=0,how='cube')
     
     if clean==True:
-        print(f'cleaning with factor {cleaning_factor}') 
+        print(f'cleaning with factor {cleaning_factor}') # removing cleaning_factor * sigma data
         noise,sigma = get_noise(cube,x,y,z,xpad,ypad,zpad)
         tolerance = sigma * cleaning_factor
         diff = moment_map.hdu.data - noise
@@ -170,7 +170,6 @@ def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='worl
         ax = fig.add_subplot(gs[0:17,0:16], projection=moment_map.wcs) # POINTS ARE NOT IN RA/DEC but the AXES ARE
         ax_HI = fig.add_subplot(gs[19:,0:20])
         hx,hy,current_vel_HI,HI_centre,HI_radius=get_HI_profile(cube, x, y, z, xpad, ypad,radius=HI_radii)
-
         # Display the HI profile
         ax_HI.plot(hx, hy,'o-', color='k')
         ax_HI.axvline(current_vel,ls='--',color='k',alpha=0.5)
@@ -185,15 +184,14 @@ def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='worl
         ax_HI.tick_params(which='major', length=8, direction='in')#,right=True)
         ax_HI.tick_params(which='minor', length=4, direction='in')#,right=True)
         
-        ax.scatter(HI_centre[0],HI_centre[1])
-        ax.scatter(sub_cube_center_x,sub_cube_center_y,color='k',marker='+')
+        #ax.scatter(HI_centre[0],HI_centre[1]) #x and y of closest pixel to the ra/dec of the object
+        ax.scatter(sub_cube_center_x,sub_cube_center_y,color='k',marker='+') #the exact x
         circle = plt.Circle(HI_centre,HI_radius,facecolor=None,color='k',fill=False,ls='--')
         #circle_half = plt.Circle(HI_centre,HI_radius/2,facecolor=None,color='k',fill=False,ls=':')
         #circle_quarter = plt.Circle(HI_centre,HI_radius/4,facecolor=None,color='k',fill=False,ls=':')
         ax.add_artist(circle)
         #ax.add_artist(circle_half)
         #ax.add_artist(circle_quarter)
-        
         
     else:
         ax = fig.add_subplot(pos,projection=moment_map.wcs)
@@ -203,14 +201,13 @@ def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='worl
         if vmin==None and vmax==None: #if both vmin & vmax are not specified:
             im = ax.imshow(img,cmap='YlOrRd') #making the 2D histogram
         elif vmin!=None and vmax==None:
-            im = ax.imshow(img,cmap='YlOrRd', vmin=vmin)
+            im = ax.imshow(img,cmap='YlOrRd', vmin=vmin) #cmap='YlOrRd' #cmap='RdBu'
         elif vmax!=None and vmin==None:
             im = ax.imshow(img,cmap='YlOrRd', vmax=vmax)
         elif vmin!=None and vmax!=None:
             im = ax.imshow(img,cmap='YlOrRd', vmin=vmin, vmax=vmax)
-        #colorbar
         cbar = plt.colorbar(im,ax=ax, pad=0.01)
-        cbar.set_label('Brightness (Jy km/s /beam)', size=10, rotation=270,labelpad=10) 
+        cbar.set_label('Brightness (Jy km/s /beam)', size=10, rotation=90,labelpad=11) 
         cbar.ax.tick_params(labelsize=10)
     elif order==1: #moment1
         if vmin==None and vmax==None: #if both vmin & vmax are not specified:
@@ -224,7 +221,6 @@ def cut_mom_map(cube,x,y,z,xpad,ypad,zpad,pos=111,label=None,ID=None,frame='worl
             im = ax.imshow(img,cmap='RdBu', vmax=vmax)
         elif vmin!=None and vmax!=None:
             im = ax.imshow(img,cmap='RdBu', vmin=vmin, vmax=vmax)
-        #colorbar
         cbar = plt.colorbar(im,ax = ax,pad=0.01)
         cbar.set_label('Velocity (km/s)', size=10, labelpad=-0.08) 
         cbar.ax.tick_params(labelsize=10)
@@ -278,15 +274,9 @@ def sum_region(centre, radius, data_array):
         val.append(data_array[ys[i]][xs[i]])
             
     sum_val = np.nansum(val)
-    
     return sum_val
-
+    
 def get_HI_profile(cube,x,y,z,xpad,ypad,radius):
-    if radius > 0:
-        pass
-    else:
-        print('no radius, using default 10 pixels ')
-        radius = 5
     current_wcs_3d = cube.wcs
     current_ra,current_dec,current_vel = current_wcs_3d.pixel_to_world_values(x,y,z)
     sub_cube = reduce_fits_file(cube, x, y, z, xpad, ypad, zpad) # Cutting the main fits file into a sub-cube
@@ -298,22 +288,23 @@ def get_HI_profile(cube,x,y,z,xpad,ypad,radius):
     pixel_radius = radius #radius/arcsec_per_pixel #nearest pixel to the desired arcsec
     z_len = len(sub_cube_data)
     xs,ys = np.ones(z_len),np.ones(z_len)
-    centre_pix = (int(np.round(centre_sub_x)),int(np.round(centre_sub_y)))
+    centre_pix = (int(np.round(centre_sub_x)),int(np.round(centre_sub_y))) # this is the central pixel of the subcube 
     zs = np.arange(z_len)
     
     _,_,current_vel_range = current_wcs_3d.pixel_to_world_values(xs,ys,zs)
     
-    current_vel_range = current_vel_range/1000
+    current_vel_range = current_vel_range
     
-    line_intensity_max = [sum_region(centre_pix,pixel_radius,z_channel) for z_channel in sub_cube_data] #loop that gives me a sum of everything in the z channel range
-    return current_vel_range,line_intensity_max,current_vel/1000, centre_pix, pixel_radius
+    line_intensity_max = [sum_region((centre_sub_x,centre_sub_y),pixel_radius,z_channel) for z_channel in sub_cube_data] #loop that gives me a sum of everything in the z channel range, in the radius around the ACTUAL centre
+    return current_vel_range/1000,line_intensity_max,current_vel/1000, centre_pix, pixel_radius
 
 ######################################
 # Plots
 ######################################
 
-pos_only=True #plot only the detections with positive velocity
-grid=False #plot moment maps in grids of 9
+pos_only = False #plot only the detections with positive velocity
+grid = True #plot moment maps in grids of 9
+sorting = False
 
 if pos_only==True:
     print("taking positives only")
@@ -324,11 +315,11 @@ if pos_only==True:
     rel=rel[pos_vels]
     ra_sof=ra_sof[pos_vels]
     dec_sof=dec_sof[pos_vels]
-    vel_sof=vel_sof[pos_vels]
-        
-arg = np.argsort(ID)[::-1]	#sort the images
-ID,x,y,z,rel = ID[arg],x[arg],y[arg],z[arg],rel[arg]
+    vel=vel[pos_vels]
 
+if sorting == True:
+    arg = np.argsort(z)[::-1]	#sort the images
+    z,x,y = z[arg],x[arg],y[arg]
 
 if grid==True:
 ### Print images in grids of 9 (mom-maps ONLY - can't accommodate HI profiles at this stage)
@@ -346,7 +337,7 @@ if grid==True:
         plt.tight_layout() 
         for counter in range(9): #a grid of 9 plots in a figure
             pos=330+counter+1
-            cut_mom_map(cube,x[i],y[i],z[i],xpad,ypad,zpad,pos=pos,label=rel[i],ID=ID[i],order=0,vmin=100,HI_radii=5)
+            cut_mom_map(cube,x[i],y[i],z[i],xpad,ypad,zpad,pos=pos,order=0,vmin=0,HI_radii=radii) #label=rel[i],ID=ID[i]
             i+=1
             if i == len(x):
                 break
@@ -354,16 +345,16 @@ if grid==True:
         plt.savefig(f'MomMaps_{file_name}_{frame_counter}of{max_frame}.png')
         frame_counter+=1
 
-else:
-### Print individual images (HI profiles optional)    
+else: # Print individual images (HI profiles optional)  
 
     print(f'printing {len(x)} moment maps')
+    print()
     for i in range(len(x)):
         fig = plt.figure(figsize=(10,10))
         plt.tight_layout()
-        cut_mom_map(cube,x[i],y[i],z[i],xpad,ypad,zpad,label=rel[i],ID=ID[i],order=0,vmin=0,HI_profile=True,HI_radii=5)
+        cut_mom_map(cube,x[i],y[i],z[i],xpad,ypad,zpad,vmin=0,HI_profile=True,HI_radii=radii)
         #remove label=rel[i],ID=ID[i] if not relevant
         plt.savefig(f'Mom_HI_{i+1}.png')
         print(f'Saved Mom_HI_{i+1}.png')
+        print() 
         plt.close()
-
